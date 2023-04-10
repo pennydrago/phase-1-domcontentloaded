6801fa3a888990c92a78b8648f660c7555a53603
Penelope Drago <prd@drago.com.au>
2023-04-10 21:10:03 +1000

    change content of DOM element

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+
+// Change DOM element content
+document.addEventListener("DOMContentLoaded", function() {
+  document.querySelector('#text').textContent = "This is really cool!";
+});
+
